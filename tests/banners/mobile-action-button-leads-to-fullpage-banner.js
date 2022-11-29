9802e38eecf3a57c8e353f2bd0e03e44500f2907
Sperling-0 <tanuja.doriya@wikimedia.de>
2022-11-29 22:14:24 +0100

    Check if closing mini banner sets the cookie
    Ticket: https://phabricator.wikimedia.org/T320732

--- tests/banners/mobile-action-button-leads-to-fullpage-banner.js
+++ tests/banners/mobile-action-button-leads-to-fullpage-banner.js
@@ -10,31 +10,35 @@ const testConfig = buildBannerTestConfig( process.argv );
 
 	await banner.waitForBanner()
 
+		.captureScreenshot( `banners/${ testConfig.bannerName }/mobile-mini-banner.png` )
+
 		// .checkIfMiniBannerHasAnimatedTextHighlight()
 		// .captureScreenshot( `banners/${ testConfig.bannerName }/mobile-mini-banner_text_highlight.png` )
-		//
+
 		// .clickMiniBannerButton()
-		// .captureScreenshot( `banners/${ testConfig.bannerName }/mobile-mini-banner.png` )
-		//
 		// .waitForFollowupBanner()
-		//
+		// .captureScreenshot( `banners/${ testConfig.bannerName }/mobile-full-page-banner.png` )
+
 		// .checkIfFullPageBannerHasAnimatedTextHighlight()
 		// .captureScreenshot( `banners/${ testConfig.bannerName }/mobile-full-page-banner_text_highlight.png` )
-		//
+
 		// .clickFollowUpBannerCloseButton()
 		// .captureScreenshot( `banners/${ testConfig.bannerName }/mobile-full-page-banner_closed.png` )
-		//
+
 		// .reload()
 		// .waitForBanner()
 		// .captureScreenshot( `banners/${ testConfig.bannerName }/mobile-mini-banner_reload.png` )
-		// .clickMiniBannerCloseButton()
-		// .captureScreenshot( `banners/${ testConfig.bannerName }/mobile-mini-banner_closed.png` )
 
-		// .reload()
-		// .waitForBanner()
+		.clickMiniBannerCloseButton()
+		.captureScreenshot( `banners/${ testConfig.bannerName }/mobile-mini-banner_closed.png` )
+		.checkCookieExists( 'centralnotice_hide_fundraising' )
+
+		.reload()
+		.waitForBanner()
 
 		.clickMiniBannerButton()
 		.waitForFollowupBanner()
+
 		.clickMonthlyPaymentInterval()
 		.captureScreenshot( `banners/${ testConfig.bannerName }/mobile-fullpage-banner_monthly_interval_selected.png` )
 		.checkPaymentTypeSofortIsDisabled()
