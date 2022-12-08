eb66635063e47450d986c83746b73d1717ad2bd4
Sperling-0 <tanuja.doriya@wikimedia.de>
2022-12-08 20:53:10 +0100

    Test: Banner does not appear on too small viewport
    - If the viewport is too small (check size issue threshold): the banner should not be loaded
    
    https://phabricator.wikimedia.org/T320732

--- tests/banners/desktop-banner-small-viewport-behavior.js
+++ tests/banners/desktop-banner-small-viewport-behavior.js
@@ -6,7 +6,12 @@ const testConfig = buildBannerTestConfig( process.argv );
 
 ( async () => {
 
-	const banner = new Banner( testConfig.url, bannerConfig.selectors, testConfig.parameters, { viewport: { width:1299, height:800 } } );
+	// Test - 01: Desktop: On big viewports there is a big text element, on small viewports the slider element is shown
+	//const banner = new Banner( testConfig.url, bannerConfig.selectors, testConfig.parameters, { viewport: { width:1299, height:800 } } );
+
+	//Test - 02: If the viewport is too small (check size issue threshold): the banner should not be loaded
+	// At width 577px banner does not appear, it starts appearing at 578px
+	const banner = new Banner( testConfig.url, bannerConfig.selectors, testConfig.parameters, { viewport: { width:577, height:800 } } );
 
 	await banner.waitForBanner()
 		.checkSliderIsDisplayedOnSmallViewPort()
